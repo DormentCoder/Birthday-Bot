b4ca88d403a985f96740efa26ebe329881fb8bfe
DormentCoder <29092866@students.lincoln.ac.uk>
2024-12-05 13:59:19 +0000

    Add files via upload

---
+++ Birthday Bot.py
@@ -0,0 +1,9 @@
+#Birthday Bot by Dc117 Corp.
+
+print('Birthday Bot V1.00')
+ans=input('Is it your birthday? (y/n)')
+if ans=='y':
+    print('Happy Birthday!')
+    print('Time for cake!')
+else:
+    print('Not Yet!')
